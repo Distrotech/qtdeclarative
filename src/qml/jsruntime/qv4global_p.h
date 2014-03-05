3c2c3acc1ada0a8dc792cf1ff101e3de6c85d362
Greg Nietsky <gregory@distrotech.co.za>
2014-03-05 08:41:52 +0200

    Apply changes for X32 accepted upstream

--- src/qml/jsruntime/qv4global_p.h
+++ src/qml/jsruntime/qv4global_p.h
@@ -71,8 +71,12 @@ inline double trunc(double d) { return d > 0 ? floor(d) : ceil(d); }
 
 #if defined(Q_PROCESSOR_X86) && !defined(__ILP32__) && (defined(Q_OS_WIN) || defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD))
 #define V4_ENABLE_JIT
-#elif defined(Q_PROCESSOR_X86_64) && !defined(__ILP32__) && (defined(Q_OS_WIN) || defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_FREEBSD))
+#elif defined(Q_PROCESSOR_X86_64) && (defined(Q_OS_WIN) || defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_FREEBSD))
+
+#if !defined(__ILP32__)
 #define V4_ENABLE_JIT
+#endif
+
 #elif defined(Q_PROCESSOR_ARM_32)
 
 #if defined(thumb2) || defined(__thumb2__) || ((defined(__thumb) || defined(__thumb__)) && __TARGET_ARCH_THUMB-0 == 4)
